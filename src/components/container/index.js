5796f39dae4d3196e37045c306f8ff4de4f44602
mchiozzi <mchiozzi95@gmail.com>
2020-10-18 15:44:00 -0300

    looks like it worked...

---
+++ src/components/container/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import DivContainer from './styles'
+
+export default function Container(){
+    return <DivContainer> <h1>mChiozzi</h1>
+    <label>Under construction</label></DivContainer>
+
+
+}
---
+++ src/components/container/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    min-width:70%;
+    
+    border-radius: 10px;
+    border-style: solid;
+    border-color: grey;
+    border-width: thin;
+
+    background: white;
+    color: black;
+`
+
+export default Container;
