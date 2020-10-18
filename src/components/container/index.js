382bfc37883204355633e403edcf962a26b2417f
mchiozzi <mchiozzi95@gmail.com>
2020-10-18 12:12:07 -0300

    revert index

--- src/components/container/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DivContainer from './styles'
-
-export default function Container(){
-    return <DivContainer> <h1>mChiozzi</h1>
-    <label>Under construction</label></DivContainer>
-
-
-}
--- src/components/profile/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import PictureFrame from './styles'
-
-import picture from '../../assets/images/profile.jpg'
-
-export default function ProfilePicture(){
-    return <PictureFrame src={picture} alt='mChiozzi'/>
-}
