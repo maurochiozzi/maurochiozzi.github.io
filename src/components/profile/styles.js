8f4385ca12fb6aa4497518c8325f98c2fca4536d
mchiozzi <mchiozzi95@gmail.com>
2020-10-18 11:37:47 -0300

    change repo

---
+++ src/components/profile/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const PictureFrame = styled.img`
+    width: 150px;
+    
+    border-radius: 50%;
+    border-style: solid;
+    border-color: grey;
+    border-width: thin;
+
+    background: white;
+`
+
+export default PictureFrame;
