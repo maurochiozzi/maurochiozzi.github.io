b46114d8d3d3c21b18b14373a7bd08dad8ec90da
mchiozzi <mchiozzi95@gmail.com>
2020-10-18 16:15:35 -0300

    create upperbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import Routes from "./routes";
+import UpperBar from "./components/upperbar";
 
 function App() {
-  return <Routes />;
+  return (
+    <div>
+      <UpperBar />
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/upperbar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UpperBar() {
+  return (
+    <div>
+      <label>mChiozzi / Home </label>
+    </div>
+  );
+}
