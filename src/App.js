2cfbae288d4c0849fa984f5721e4969d12548506
mchiozzi <mchiozzi95@gmail.com>
2020-10-18 16:17:57 -0300

    create footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+
 import "./App.css";
+
 import Routes from "./routes";
 import UpperBar from "./components/upperbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <UpperBar />
       <Routes />
+      <Footer />
     </div>
   );
 }
