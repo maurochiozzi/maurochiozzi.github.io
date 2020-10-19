ada26fbe9ea5b47d2f12e316817bc298ad8d5e22
mchiozzi <mchiozzi95@gmail.com>
2020-10-18 22:08:59 -0300

    fix margin top on main content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
-import Routes from "./routes";
+import Bar from "./components/bar";
+import Footer from "./components/footer";
+
+import About from "./pages/about";
+import Home from "./pages/home";
+import NotFound from "./pages/notfound";
 
 function App() {
   return (
-    <div>
-      <Routes />
-    </div>
+    <BrowserRouter>
+      <Bar />
+
+      <div className="app-main-content">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+
+      <Footer />
+    </BrowserRouter>
   );
 }
 
