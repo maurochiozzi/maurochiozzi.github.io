a8afc46c68e45909df78c7a6364c8062c450c1e2
mchiozzi <mchiozzi95@gmail.com>
2020-10-20 07:59:56 -0300

    Updates

---
+++ precache-manifest.5c7d85ea4565cbc285a12ae1d4fe17bc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e085845ba1a10fff1230e0bb6767b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c90db84e71a8b6308f9",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "b47139699bf33cbe5842",
+    "url": "/static/css/main.db519ecc.chunk.css"
+  },
+  {
+    "revision": "8c90db84e71a8b6308f9",
+    "url": "/static/js/2.9906a26f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9906a26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47139699bf33cbe5842",
+    "url": "/static/js/main.093c42f1.chunk.js"
+  },
+  {
+    "revision": "38c862aa223c3fec5378",
+    "url": "/static/js/runtime-main.6ef29a8f.js"
+  },
+  {
+    "revision": "82dc7199ebc0084cdc456effa2010405",
+    "url": "/static/media/loadcell-1.82dc7199.JPG"
+  },
+  {
+    "revision": "96529bd64b61fef3b679ca44bb07bd6f",
+    "url": "/static/media/loadcell-2.96529bd6.JPG"
+  },
+  {
+    "revision": "824a97c0af2eb35e4647a3b5340c8917",
+    "url": "/static/media/loadcell-3.824a97c0.JPG"
+  },
+  {
+    "revision": "64a406d70e3ab4ba851c67e1e8a14837",
+    "url": "/static/media/loadcell-4.64a406d7.png"
+  },
+  {
+    "revision": "997d83643b66e75913048ee7c58d29fd",
+    "url": "/static/media/loadcell-5.997d8364.png"
+  },
+  {
+    "revision": "554250002c8e5f4ebfe8c69a4c2c4597",
+    "url": "/static/media/loadcell-6.55425000.png"
+  },
+  {
+    "revision": "76713cec0d1a33a478c115d8fb3fba07",
+    "url": "/static/media/loadcell-7.76713cec.JPG"
+  },
+  {
+    "revision": "86223839f268ed93bd0c8914c9d47f61",
+    "url": "/static/media/loadcell-8.86223839.jpg"
+  },
+  {
+    "revision": "3bd7906bd5bbf4d97713eb0e6cc2bdb8",
+    "url": "/static/media/loadcell-9.3bd7906b.jpg"
+  },
+  {
+    "revision": "e0bddd5f49b980fd8f68609577af7652",
+    "url": "/static/media/loadcell.e0bddd5f.jpg"
+  },
+  {
+    "revision": "dc6cc2e6e26d5a3521d1e7793cf5ada1",
+    "url": "/static/media/mChiozzi.dc6cc2e6.pdf"
+  },
+  {
+    "revision": "4bbd883927ede7f5ca3bdc7a0ff19d6c",
+    "url": "/static/media/notfound.4bbd8839.jpg"
+  },
+  {
+    "revision": "f2887b888a260d1789f0d72548dc7680",
+    "url": "/static/media/window.f2887b88.jpg"
+  }
+]);
