b5a46f26e4b172f27da751cbcdaece0fec4f9b4d
mchiozzi <mchiozzi95@gmail.com>
2020-10-20 14:16:30 -0300

    add embraer intership

--- src/assets/projects/embraer/index.js
+++ src/assets/projects/embraer/index.js
@@ -1,16 +1,16 @@
 export const Embraer = {
   id: 2,
-  title: "Intership: Embraer S/A",
-  caption:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia mattis purus vitae tincidunt.",
+  title: "Internship: Embraer",
+  caption: "Internship with the Embraer Quality Engineering Team.",
   thumbnail: "",
   text: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dolor a tortor condimentum aliquam. Nullam facilisis eget neque eget lobortis. Suspendisse quis tempus ipsum. Nunc sed laoreet ex. Fusce eget sapien ut risus iaculis congue. Aenean libero metus, pharetra quis sagittis ac, porta faucibus ante. Mauris iaculis ex dui, eget cursus tellus tincidunt ut. Nulla egestas, ex fermentum volutpat finibus, odio ex auctor dolor, imperdiet vehicula lectus augue et risus. Nullam ultrices elit sapien, non commodo eros rutrum non. Nam eget malesuada turpis. Ut tincidunt neque a orci tincidunt, vel varius dolor semper. Proin vel elementum erat. Aenean rutrum sagittis nisi, et pharetra tortor maximus et.",
-    "In pellentesque mauris in velit interdum lobortis. Integer tristique tincidunt quam non mattis. In hac habitasse platea dictumst. Phasellus condimentum, nunc id efficitur cursus, justo mi dictum enim, in placerat mi odio sed nibh. Praesent placerat, est et laoreet aliquet, sem nulla convallis ipsum, quis posuere velit augue non odio. Sed viverra mollis quam sit amet lobortis. Proin dapibus lectus vitae aliquam pulvinar. Suspendisse eu rhoncus augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla tincidunt ligula non nunc porta, ullamcorper porta eros volutpat.",
-    "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas justo urna, molestie sed tempus imperdiet, ullamcorper eget diam. Duis tempus accumsan arcu, a vehicula tellus. Integer quis est finibus, venenatis justo ut, blandit libero. Nam in posuere nulla. Nullam at lorem sed eros condimentum imperdiet. Vivamus ipsum odio, molestie vitae efficitur nec, sagittis sed neque. Quisque euismod, lacus ut tempor congue, est est convallis eros, quis mattis lacus augue ut sem.",
+    "Besides being the second internship during graduation, Embraer Internship was the first contact with the industry and either with the Quality Culture.",
+    "During the internship program, a lot of projects were developed, going through process automation using Python, Web/SAP scrapping, to process management improvements using web development.",
+    "The main tasks during this internship were the development of new scripts to reduce time spent doing repetitive tasks, quality process follow-up (Corrective Actions and Non-conformity analysis), and development of new systems to help Quality Team gain performance and improve product quality, focusing also on non-conformity reduction.",
+    "Thanks to Embraer Internship, the introduction and experience with Quality Process were singular, showing how people are the key resources to achieve spectacular goals. See the source to access Embraer Main page and learn more about this company.",
   ],
-  source: "http://monografias.ufrn.br/handle/123456789/10689",
+  source: "https://embraer.com/global/en",
   key_developers: ["Mauro Chiozzi"],
-  tools: ["Ansys", "SolidWorks", "Proteus Ares & Iris", "Python", "Embedded C"],
+  tools: ["Python", "SAP Script", "Quality Tools"],
   pictures: [],
 };
