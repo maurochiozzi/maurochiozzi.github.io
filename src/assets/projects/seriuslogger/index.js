bd909a88beeedecf8990606385e874758625633c
mchiozzi <mchiozzi95@gmail.com>
2020-10-24 00:41:20 -0300

    fix seriouslogger name

--- src/assets/projects/seriuslogger/index.js
+++ src/assets/projects/seriuslogger/index.js
@@ -1,10 +1,10 @@
 export const seriuslogger = {
   id: 6,
-  title: "SeriusLogger",
-  caption: "Serius datalogger for times when things get serious... Or not.",
+  title: "SeriousLogger",
+  caption: "Serious datalogger for times when things get serious... Or not.",
   thumbnail: require("./tmb.png"),
   text: [""],
-  source: "https://github.com/maurochiozzi/seriuslogger",
+  source: "https://github.com/maurochiozzi/seriouslogger",
   key_developers: ["Mauro Chiozzi"],
   tools: ["Python", "Serial Data Logger", "Git"],
   pictures: [],
