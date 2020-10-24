5aa69984eccb19257315ff27aeb064efe4618b9a
mchiozzi <mchiozzi95@gmail.com>
2020-10-23 23:01:58 -0300

    update about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -18,47 +18,57 @@ export default function About() {
       <div className="main-content">
         <p>
           I'm Mechanical <span className="highlight">[Engineer]</span> and
-          passionate for overall engineering. For a while, I've been developing
-          solutions in many applications fields. Like developing mechanical
-          solutions, I enjoy a lot of coding using many different paradigms, and
-          by that, I started teaching programming logic on my nineteens.
+          passionate for overall engineering, and I might say it's because all
+          my creative developed as a child during my joyful plays with{" "}
+          <span className="highlight">[Lego]</span>.
         </p>
         <p>
-          As the time past by, I started to expand my knowledge and also going
-          deeper. Nowadays, I develop solutions more focused on embedded
-          systems, going through hardware development to software (embedded or
-          not).
-        </p>
-        <p>
-          My mission is to help people achieve their goals by sharing knowledge
-          between both parts and by managing to lead then to key results.
-        </p>
-        <hr />
-        <p>
-          I've Bachelor degree in Science & Technology, with emphasis on
-          Mechanic Technologist, and also Mechanical Engineering degree, both by{" "}
+          To keep fill-fulling my need for creative, I enrolled on{" "}
           <a
             href="https://www.ufrn.br/en"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Federal University of Rio Grande do Norte
-          </a>
+            <span className="highlight">[UFRN]</span>
+          </a>{" "}
+          where I first graduate as Bachelor in Science & Technology. The first
+          year I had my first programming class: it was love on the first sight.
+          The following year I started lessoning programming and nowadays I
+          still developing my <span className="highlight">[coding skills]</span>
           .
         </p>
         <p>
-          If you want, you can download my{" "}
+          Later on, after finishing the Bachelor course, I enrolled on the
+          Mechanical Engineering course, where I could develop more skills in
+          the area that I'm passionate about. Also, team-development was
+          possible as well as a development in problem-solving with the hard
+          work time active with the{" "}
+          <span className="highlight">[Car-Kará]</span> Baja SAE and AeroDesign
+          SAE <span className="highlight">[Teams]</span>.
+        </p>
+        <p>
+          In general, I'm highly motived by the daily challenges and the
+          knowledge awarded aside with the{" "}
+          <span className="highlight">[goals achieved]</span>. My mission is,
+          besides the self-development, is to{" "}
+          <span className="highlight">[help people]</span> achieve their goals
+          by sharing knowledge between both parts and by managing to lead then
+          (or us) to <span className="highlight">[key results]</span>.
+        </p>
+        <p>
+          A lot of things happened during those academic years, and some of then
+          you can find out by downloading my{" "}
           <a href={Resume} target="_blank" rel="noopener noreferrer">
             <span className="highlight">[resume here]</span>
           </a>
-          , or find it in the bottom-left among the{" "}
-          <span className="highlight">[social networks]</span> links and{" "}
-          <span className="highlight">[e-mail]</span> for contact. For some
-          projects developed by myself, checkout the{" "}
+          ; visiting the{" "}
           <Link to="/projects">
             <span className="highlight">[projects portfolio]</span>
-          </Link>{" "}
-          page.
+          </Link>
+          ; exploring a little more on my{" "}
+          <span className="highlight">[social networks]</span>; or feel free to
+          send an <span className="highlight">[e-mail]</span> (see bottom-left
+          footer).
         </p>
       </div>
     </Content>
