38d62b1a43cc23a89659ff69df3fc4817092bf69
mchiozzi <mchiozzi95@gmail.com>
2020-10-23 23:26:27 -0300

    add poject images

--- src/assets/projects/sevenvelo/index.js
+++ src/assets/projects/sevenvelo/index.js
@@ -8,5 +8,21 @@ export const sevenvelo = {
   source: "https://github.com/maurochiozzi/7-segment-velocimeter",
   key_developers: ["Mauro Chiozzi"],
   tools: ["Embedded C", "Proteus Iris & Ares"],
-  pictures: [],
+  pictures: [
+    {
+      id: 1,
+      src: require("./7v-1.jpg"),
+      legend: "Code logic on hardware bench test",
+    },
+    {
+      id: 2,
+      src: require("./7v-2.PNG"),
+      legend: "Circuit on study schematic",
+    },
+    {
+      id: 3,
+      src: require("./7v-3.png"),
+      legend: "Embedded C logic",
+    },
+  ],
 };
