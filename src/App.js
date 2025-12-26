b597d4d9aa75f068ee60594315d5313a8711dcbd
Mauro Chiozzi <mchiozzi95@gmail.com>
2025-12-26 01:09:05 +0100

    implement react 18 optimizations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { Suspense, lazy } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import "./App.css";
 
 import Bar from "./components/bar";
 import Footer from "./components/footer";
 
-import About from "./pages/about";
-import Home from "./pages/home";
-import Projects from "./pages/projects";
-import NotFound from "./pages/notfound";
+const About = lazy(() => import("./pages/about"));
+const Home = lazy(() => import("./pages/home"));
+const Projects = lazy(() => import("./pages/projects"));
+const NotFound = lazy(() => import("./pages/notfound"));
 
 export default function App() {
   return (
@@ -16,12 +19,26 @@ export default function App() {
       <Bar />
 
       <div className="app-main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "2rem",
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </div>
 
       <Footer />
