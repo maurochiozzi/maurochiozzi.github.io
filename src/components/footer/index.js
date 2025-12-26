78bcad8fbd382272de2a1fcfa4dd23207704dea5
Mauro Chiozzi <mchiozzi95@gmail.com>
2025-12-26 04:38:12 +0100

    icons change

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,6 +3,19 @@ import { ThemeContext } from "../../contexts/ThemeContext";
 import { Footer as FooterContainer } from "./styles";
 import Switch from "@mui/material/Switch";
 import { styled, useTheme } from "@mui/material/styles";
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaEnvelope,
+  FaFileDownload,
+} from "react-icons/fa";
+const LightTooltip = withStyles(() => ({
+  tooltip: {
+    fontSize: 13,
+  },
+}))(Tooltip);
 
 const ThemeSwitch = styled(Switch)(({ theme }) => ({
   width: 54,
@@ -60,16 +73,38 @@ export default function Footer() {
 
   return (
     <FooterContainer>
-      <a href="https://github.com/maurochiozzi" target="_blank" rel="noreferrer">
-        GitHub
-      </a>
-      <a
-        href="https://www.linkedin.com/in/maurochiozzi/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LinkedIn
-      </a>
+      <LightTooltip title="GitHub" arrow>
+        <a
+          href={"https://github.com/maurochiozzi"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </LightTooltip>
+      <LightTooltip title="LinkedIn" arrow>
+        <a
+          href={"https://www.linkedin.com/in/maurochiozzi/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+      </LightTooltip>
+      <LightTooltip title="e-mail" arrow>
+        <a
+          href={"mailto: mchiozzi95@gmail.com"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaEnvelope />
+        </a>
+      </LightTooltip>
+      <LightTooltip title="Download Resume" arrow>
+        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+          <FaFileDownload />
+        </a>
+      </LightTooltip>
       <ThemeSwitch
         checked={theme.palette.mode === "dark"}
         onChange={toggleTheme}
