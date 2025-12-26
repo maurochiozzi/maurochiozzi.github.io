3c22070ba2864b4dfeb7dd1169efe05cbfcfba6e
Mauro Chiozzi <mchiozzi95@gmail.com>
2025-12-26 04:10:13 +0100

    improving navbar and footer

--- src/components/bar/index.js
+++ src/components/bar/index.js
@@ -1,24 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { IconButton } from "@mui/material";
-import { Brightness4, Brightness7 } from "@mui/icons-material";
-import { useTheme } from "@mui/material/styles";
 
-import { ThemeContext } from "../../contexts/ThemeContext";
 import { Nav } from "./styles";
 
 export default function Bar() {
-  const theme = useTheme();
-  const { toggleTheme } = useContext(ThemeContext);
-
   return (
     <Nav>
       <Link to="/">mChiozzi</Link>
       <Link to="/about">About</Link>
-      <Link to="/projects">Awesome Projects!</Link>
-      <IconButton onClick={toggleTheme} color="inherit">
-        {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
-      </IconButton>
+      <Link to="/projects">The Lab!</Link>
     </Nav>
   );
 }
